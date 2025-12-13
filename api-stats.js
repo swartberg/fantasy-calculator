741c6f2d87b45d19dcc65707924c179ecba46da9
swartberg <141254819+swartberg@users.noreply.github.com>
2025-12-13 23:47:32 +0200

    reworked api data results and added data import to dom manipulation script. Created UI to display fetched data for player stats

--- api-stats.js
+++ api-stats.js
@@ -1,231 +1,216 @@
-export async function fetchAndUpdate() {
-    fetch("https://live.euroleague.net/api/PlaybyPlay?gamecode=104&seasoncode=E2025")
-        .then(response => response.json())
-        .then(data => {
-            const players = {};
-            const allPlays = [
-                ...(data.FirstQuarter || []),
-                ...(data.SecondQuarter || []),
-                ...(data.ThirdQuarter || []),
-                ...(data.ForthQuarter || []),
-                ...(data.ExtraTime || []),
-            ];
-            
-            allPlays.forEach(play => {
-                const id = play.PLAYER_ID;
-                const name = play.PLAYER;
-                const team = play.CODETEAM?.trim();
+export async function fetchAndUpdate(gameCode) {
+    try{
+        const response = await fetch(`https://live.euroleague.net/api/PlaybyPlay?gamecode=${gameCode}&seasoncode=E2025`);
+        const data = await response.json();
+        const players = {};
 
-                if (!id || !name) return;
+        const allPlays = [
+            ...(data.FirstQuarter || []),
+            ...(data.SecondQuarter || []),
+            ...(data.ThirdQuarter || []),
+            ...(data.ForthQuarter || []),
+            ...(data.ExtraTime || []),
+        ];
+   
+        allPlays.forEach(play => {
+            const id = play.PLAYER_ID;
+            const name = play.PLAYER;
+            const team = play.CODETEAM?.trim();
 
-                if(!players[id]) {
-                    players[id] = {
-                        id: id,
-                        name: name,
-                        team: team,
-                        stats: {
-                            points: 0,
-                            made_2pt: 0,
-                            made_3pt: 0,
-                            made_fts: 0,
-                            def_rebounds: 0,
-                            off_rebounds: 0,
-                            total_rebounds: 0,
-                            assists: 0,
-                            steals: 0,
-                            blocks: 0,
-                            draw_fouls: 0,
-                            missed_2pt: 0,
-                            missed_3pt: 0,
-                            missed_fts: 0,
-                            get_blocks: 0,
-                            turnovers: 0,
-                            fouls: 0
-                        },
-                        fantasy: 0
-                    }
-                }
-            });
+            if (!id || !name) return;
 
-            // stats calculation
-            allPlays.forEach(play => {
-                const player = players[play.PLAYER_ID];
-                if (!player) return;
-
-                const action = play.PLAYTYPE;
-                if (!action) return;
-
-                // Stats and fantasy points calculation
-                if (action === "2FGM") {
-                    player.stats.made_2pt += 1;
-                    player.stats.points += 2;
-                    player.fantasy += 2;
+            if(!players[id]) {
+                players[id] = {
+                    id: id,
+                    name: name,
+                    team: team,
+                    stats: {
+                        points: 0,
+                        made_2pt: 0,
+                        made_3pt: 0,
+                        made_fts: 0,
+                        def_rebounds: 0,
+                        off_rebounds: 0,
+                        total_rebounds: 0,
+                        assists: 0,
+                        steals: 0,
+                        blocks: 0,
+                        draw_fouls: 0,
+                        missed_2pt: 0,
+                        missed_3pt: 0,
+                        missed_fts: 0,
+                        get_blocks: 0,
+                        turnovers: 0,
+                        fouls: 0
+                    },
+                    fantasy: 0
                 }
-                else if (action === "2FGA") {
-                    player.stats.missed_2pt += 1;
-                    player.fantasy -= 1;
-                }
-                else if (action === "3FGM") {
-                    player.stats.made_3pt += 1;
-                    player.stats.points += 3;
-                    player.fantasy += 3;
-                }
-                else if (action === "3FGA") {
-                    player.stats.missed_3pt += 1;
-                    player.fantasy -= 1;
-                }
-                else if (action === "FTM") {
-                    player.stats.made_fts += 1;
-                    player.stats.points += 1;
-                    player.fantasy += 1;
-                }
-                else if (action === "FTA") {
-                    player.stats.missed_fts += 1;
-                    player.fantasy -= 1;
-                }
-                // def reb
-                else if (action === "D") {
-                    player.stats.def_rebounds += 1;
-                    player.fantasy += 1;
-                    player.stats.total_rebounds += 1;
-                }
-                // off reb
-                else if (action === "O") {
-                    player.stats.off_rebounds += 1;
-                    player.fantasy += 1.5;
-                    player.stats.total_rebounds += 1;
-                }
-                // assist
-                else if (action === "AS") {
-                    player.stats.assists += 1;
-                    player.fantasy += 1.5;
-                }
-                // steal
-                else if (action === "ST") {
-                    player.stats.steals += 1;
-                    player.fantasy += 1.5;
-                }
-                // block
-                else if (action === "FV") {
-                    player.stats.blocks += 1;
-                    player.fantasy += 1;
-                }
-                // drawn fouls
-                else if (action === "RV") {
-                    player.stats.draw_fouls += 1;
-                    player.fantasy += 1;
-                }
-                else if (action === "TO") {
-                    player.stats.turnovers += 1;
-                    player.fantasy -= 1.5;
-                }
-                // blocks received
-                else if (action === "AG") {
-                    player.stats.get_blocks += 1;
-                    player.fantasy -= 0.5;
-                }
-                // fouls
-                else if (action === "CM") {
-                    player.stats.fouls += 1;
-                    if (player.stats.fouls >= 5){
-                        player.fantasy -= 5;
-                    }
-                }
-            })
-
-            if (data.Live === true) {
-                // console.clear();
-                console.table(Object.values(players).map(p => ({
-                    Name: p.name,
-                    Team: p.team,
-                    Points: p.stats.points,
-                    Rebounds: p.stats.total_rebounds,
-                    Assists: p.stats.assists,
-                    Fantasy_Points: p.fantasy
-                })));
-
-                const playerCount = Object.values(players).length;
-                const fantasyInfo = [];
-
-                for (let i = 0; i < playerCount; i++) {
-                    const playerName = Object.values(players)[i].name;
-                    const playerFantasyPoints = Object.values(players)[i].fantasy;
-
-                    fantasyInfo.push({ name: playerName, fantasy_points: playerFantasyPoints});
-                }
-                return fantasyInfo;  
             }
+        });
 
-            if (data.Live === false) {
-                clearInterval(updateLoop);
-                const teamTotal = {};
+        // stats calculation
+        allPlays.forEach(play => {
+            const player = players[play.PLAYER_ID];
+            if (!player) return;
 
-                // team total points calculation
-                for (const id in players) {
-                    const player = players[id];
-                    const team = player.team;
+            const action = play.PLAYTYPE;
+            if (!action) return;
 
-                    if (!teamTotal[team]) {
-                        teamTotal[team] = { points: 0}
-                    }
-                    
-                    teamTotal[team].points += player.stats.points;
-
+            // Stats and fantasy points calculation
+            if (action === "2FGM") {
+                player.stats.made_2pt += 1;
+                player.stats.points += 2;
+                player.fantasy += 2;
+            }
+            else if (action === "2FGA") {
+                player.stats.missed_2pt += 1;
+                player.fantasy -= 1;
+            }
+            else if (action === "3FGM") {
+                player.stats.made_3pt += 1;
+                player.stats.points += 3;
+                player.fantasy += 3;
+            }
+            else if (action === "3FGA") {
+                player.stats.missed_3pt += 1;
+                player.fantasy -= 1;
+            }
+            else if (action === "FTM") {
+                player.stats.made_fts += 1;
+                player.stats.points += 1;
+                player.fantasy += 1;
+            }
+            else if (action === "FTA") {
+                player.stats.missed_fts += 1;
+                player.fantasy -= 1;
+            }
+            // def reb
+            else if (action === "D") {
+                player.stats.def_rebounds += 1;
+                player.fantasy += 1;
+                player.stats.total_rebounds += 1;
+            }
+            // off reb
+            else if (action === "O") {
+                player.stats.off_rebounds += 1;
+                player.fantasy += 1.5;
+                player.stats.total_rebounds += 1;
+            }
+            // assist
+            else if (action === "AS") {
+                player.stats.assists += 1;
+                player.fantasy += 1.5;
+            }
+            // steal
+            else if (action === "ST") {
+                player.stats.steals += 1;
+                player.fantasy += 1.5;
+            }
+            // block
+            else if (action === "FV") {
+                player.stats.blocks += 1;
+                player.fantasy += 1;
+            }
+            // drawn fouls
+            else if (action === "RV") {
+                player.stats.draw_fouls += 1;
+                player.fantasy += 1;
+            }
+            else if (action === "TO") {
+                player.stats.turnovers += 1;
+                player.fantasy -= 1.5;
+            }
+            // blocks received
+            else if (action === "AG") {
+                player.stats.get_blocks += 1;
+                player.fantasy -= 0.5;
+            }
+            // fouls
+            else if (action === "CM") {
+                player.stats.fouls += 1;
+                if (player.stats.fouls >= 5){
+                    player.fantasy -= 5;
                 }
-
-                // winning team calculation
-                const teamsPlayed = Object.keys(teamTotal);
-                const teamOneScore = teamTotal[teamsPlayed[0]].points;
-                const teamTwoScore = teamTotal[teamsPlayed[1]].points;
-                let winningTeam;
-
-                if (teamOneScore > teamTwoScore) {
-                     winningTeam = teamsPlayed[0];
-                }
-                else {
-                    winningTeam = teamsPlayed[1];
-                }
-                console.log('Winner is:', winningTeam);
-
-                // end game calculations
-                for (id in players) {
-                    const player = players[id];
-                    const team = player.team;
-
-                    if ((player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.assists >= 10 && player.stats.steals >= 10)
-                    || (player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.assists >= 10 && player.stats.blocks >= 10)) {
-                        player.fantasy += 100;
-                    }
-                    else if ((player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.assists >= 10)
-                    || (player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.blocks >= 10)
-                    || (player.stats.points >= 10 && player.stats.assists >= 10 && player.stats.steals >= 10)
-                    || (player.stats.total_rebounds >= 10 && player.stats.assists >= 10 && player.stats.blocks >= 10)) {
-                        player.fantasy += 30;
-                    }
-                    else if ((player.stats.points >= 10 && player.stats.total_rebounds >= 10)
-                    || (player.stats.points >= 10 && player.stats.assists >= 10)
-                    || (player.stats.total_rebounds >= 10 && player.stats.assists >= 10)) {
-                        player.fantasy += 10;
-                    }
-
-                    if (team === winningTeam) {
-                        player.fantasy += 1.5;
-                    }
-                    else {
-                        player.fantasy -= 1.5;
-                    }
-                }
-                console.table(Object.values(players).map(p => ({
-                    Name: p.name,
-                    Fantasy_Points: p.fantasy
-                })));
             }
         })
-        .catch(error => {
-            console.error("Error fetching API:", error);
-        });
+
+        if (data.Live === false) {
+            const teamTotal = {};
+
+            // team total points calculation
+            for (const id in players) {
+                const player = players[id];
+                const team = player.team;
+
+                if (!teamTotal[team]) {
+                    teamTotal[team] = { points: 0}
+                }
+                
+                teamTotal[team].points += player.stats.points;
+            }
+
+            // winning team calculation
+            const teamsPlayed = Object.keys(teamTotal);
+            const teamOneScore = teamTotal[teamsPlayed[0]].points;
+            const teamTwoScore = teamTotal[teamsPlayed[1]].points;
+            let winningTeam;
+
+            if (teamOneScore > teamTwoScore) {
+                    winningTeam = teamsPlayed[0];
+            }
+            else {
+                winningTeam = teamsPlayed[1];
+            }
+            console.log('Winner is:', winningTeam);
+
+            // end game calculations
+            for (const id in players) {
+                const player = players[id];
+                const team = player.team;
+
+                if ((player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.assists >= 10 && player.stats.steals >= 10)
+                || (player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.assists >= 10 && player.stats.blocks >= 10)) {
+                    player.fantasy += 100;
+                }
+                else if ((player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.assists >= 10)
+                || (player.stats.points >= 10 && player.stats.total_rebounds >= 10 && player.stats.blocks >= 10)
+                || (player.stats.points >= 10 && player.stats.assists >= 10 && player.stats.steals >= 10)
+                || (player.stats.total_rebounds >= 10 && player.stats.assists >= 10 && player.stats.blocks >= 10)) {
+                    player.fantasy += 30;
+                }
+                else if ((player.stats.points >= 10 && player.stats.total_rebounds >= 10)
+                || (player.stats.points >= 10 && player.stats.assists >= 10)
+                || (player.stats.total_rebounds >= 10 && player.stats.assists >= 10)) {
+                    player.fantasy += 10;
+                }
+
+                if (team === winningTeam) {
+                    player.fantasy += 1.5;
+                }
+                else {
+                    player.fantasy -= 1.5;
+                }
+            }
+            console.table(Object.values(players).map(p => ({
+                Name: p.name,
+                Fantasy_Points: p.fantasy
+            })));
+        }
+        return {
+            players: Object.values(players).map(p => ({
+                Name: p.name,
+                Team: p.team,
+                Points: p.stats.points,
+                Rebounds: p.stats.total_rebounds,
+                Assists: p.stats.assists,
+                Turnovers: p.stats.turnovers,
+                Fantasy_Points: p.fantasy
+        })),
+            Live: data.Live === true,  
+    };
+    }
+    catch (error) {
+        console.error("Error fetching API:", error);
+        return [];
+    }
 }
-
-fetchAndUpdate();
-
-const updateLoop = setInterval(fetchAndUpdate, 20000);
--- dom.js
+++ dom.js
@@ -1,12 +1,129 @@
-import { fetchAndUpdate }  from "./api-stats.js";
+import { fetchAndUpdate } from "./api-stats.js";
 
-console.log('dom.js is running');
+const selectGameBtn = document.querySelector(".js-select-game");
+let updateLoop = null;
 
-const fPointsDisplay = document.querySelector('.player-fantasy-points');
-console.log('element found:', fPointsDisplay);
-const fPoints = fetchAndUpdate();
-console.log('function result:', fPoints);
+selectGameBtn.addEventListener("click", () => {
+    const gameCode = prompt("Enter game number:");
 
-console.log(fPoints);
+    if (!gameCode) return;
 
-fPointsDisplay.textContent = fPoints[0].fantasy_points;
+    console.log("Selected game:", gameCode);
+
+    localStorage.setItem("gameCode", gameCode);
+
+    loadGame(gameCode);
+});
+
+async function loadGame(gameCode) {
+    if (updateLoop) {
+        clearInterval(updateLoop);
+        updateLoop = null;
+    }
+
+    await getStats(gameCode);
+}
+
+const TEAM_NAMES = {
+    MCO: "AS Monaco",
+    RED: "Crvena Zvezda Belgrade",
+    ZAL: "Žalgiris Kaunas",
+    PAN: "Panathinaikos Athens",
+    MAD: "Real Madrid",
+    PRS: "Paris Basketball",
+    PAM: "Valencia Basket",
+    TEL: "Maccabi Tel Aviv",
+    BAS: "Baskonia Vitoria-Gasteiz",
+    ULK: "Fenerbahce Istanbul",
+    HTA: "Hapoel Tel Aviv",
+    OLY: "Olympiacos Piraeus",
+    IST: "Anadolu Efes Istanbul",
+    MUN: "Bayern Munich",
+    DUB: "Dubai Basketball",
+    MIL: "Emporio Armani Milan",
+    BAR: "FC Barcelona",
+    VIR: "Virtus Bologna",
+    ASV: "ASVEL Villeurbanne",
+    PAR: "Partizan Belgrade",
+}
+
+async function getStats(gameCode) {
+    const result = await fetchAndUpdate(gameCode);
+    if(!result || result.players.length === 0) return;
+
+    const players = result.players;
+    const isLive = result.Live;
+
+    const teams = [...new Set(players.map(p => p.Team))];
+    const homeTeamCode = teams[0];
+    const awayTeamCode = teams[1];
+
+    document.querySelector(".js-home-team-name").textContent = TEAM_NAMES[homeTeamCode] || homeTeamCode;
+    document.querySelector(".js-away-team-name").textContent = TEAM_NAMES[awayTeamCode] || awayTeamCode;
+
+    const homeContainer = document.querySelector(".home-team");
+    const awayContainer = document.querySelector(".away-team");
+
+    homeContainer.innerHTML = "";
+    awayContainer.innerHTML = "";
+
+    players.forEach(player => {
+        const playerName = (player.Name).split(",");
+        console.log(playerName)
+        const playerTab = document.createElement("div");
+        playerTab.className = "player-tab";
+
+        playerTab.innerHTML = `
+            <div class="player-info">
+                <div class="player-name">
+                    <span class="js-player-name">${playerName[1][1]}. ${playerName[0]}</span>
+                </div>
+            </div>
+            <div class="player-stats">
+                <div class="player-pts stat-pair">
+                    <span class="stat-label">PTS</span>
+                    <span class="js-stat">${player.Points}</span>
+                </div>
+                <div class="player-reb stat-pair">
+                    <span class="stat-label">REB</span>
+                    <span class="js-stat">${player.Rebounds}</span>
+                </div>
+                <div class="player-ast stat-pair">
+                    <span class="stat-label">AST</span>
+                    <span class="js-stat">${player.Assists}</span>
+                </div>
+                <div class="player-to stat-pair">
+                    <span class="stat-label">TO</span>
+                    <span class="js-stat">${player.Turnovers}</span>
+                </div>
+            </div>
+            <div class="player-fpts stat-pair">
+                <span class="stat-label">FPTS</span>
+                <span class="js-stat js-fpts">${player.Fantasy_Points}</span>
+            </div>
+        `;
+            
+        if (player.Team === homeTeamCode) {
+            homeContainer.appendChild(playerTab);
+        }
+        else if (player.Team === awayTeamCode) {
+            awayContainer.appendChild(playerTab);
+        }
+    });
+
+    if (isLive && !updateLoop) {
+        updateLoop = setInterval(() => {
+            getStats(gameCode);
+        }, 20000);
+    }
+
+    if (!isLive && updateLoop) {
+        clearInterval(updateLoop);
+        updateLoop = null;
+    }
+}
+
+const savedGame = localStorage.getItem("gameCode");
+if(savedGame) {
+    loadGame(savedGame);
+}
